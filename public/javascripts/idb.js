9dd5a89e343ed811cff79942366ebe52f207b879
Humphrey Ahn <ahnsv@bc.edu>
2019-06-01 19:03:00 +0900

    db populate

--- public/javascripts/idb.js
+++ public/javascripts/idb.js
@@ -1,15 +1,25 @@
-import {openDB, deleteDB, wrap, unwrap} from 'idb';
+const db = new Dexie("schedule");
 
-async function idb() {
-    const db = openDB('schedules', 1, {
-        upgrade(database, oldVersion, newVersion, transaction) {
-            console.log(transaction);
-        },
-        blocked() {
-        },
-        blocking() {
-        }
-    })
+db.version(1).stores({schedules: "++id, title, start, end, category, important"});
+
+db.transaction('rw', db.schedules, async () => {
+//   populate db
+    await db.table('schedules').clear();
+    let i = 1
+    while (i < 30) {
+        const date = new Date(2019, 5, i);
+        const endDate = new Date(2019, 5, i + 1)
+        await db.schedules.add({
+            title: 'test' + i,
+            start: date.toISOString().split('T')[0],
+            end: endDate.toISOString().split('T')[0],
+            category: 'military',
+            important: true
+        })
+        i++;
+    }
+}).catch((err) => {
+    console.log(err)
+});
 
 
-}
